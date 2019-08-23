daade02dd520cb1e0414c29b26db95804003d036
Ryan S <planetlucid@gmail.com>
2019-08-23 21:38:48 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -10,3 +10,13 @@ end
   
   
 #Understanding the scope within a method of modifying and returning objects is crucial
+
+def my_collect(array)
+  i = 0
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i+=1
+  end
+  collect
+end
