e68eb98e562ad30948768122e3b040a55d1d776e
Ryan S <planetlucid@gmail.com>
2019-08-23 21:28:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+require 'pry'
 
-
+#binding.pry
+def my_collect(collection)
+  collection=[]
+  collection.find do |lang|
+    lang.upcase
+  end
+end
+  
+  
+#Understanding the scope within a method of modifying and returning objects is crucial
